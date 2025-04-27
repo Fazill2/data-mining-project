1939783d629c5c24f6c00a338845c036f373a5b4
Jakub Torłop <kubatorlop@gmail.com>
2025-04-27 21:44:12 +0200

    Build profiles for all users at once

--- src/user_profile.py
+++ src/user_profile.py
@@ -4,17 +4,15 @@ import numpy as np
 
 class UserProfileCreator:
     @staticmethod
-    def build_user_profile(movie_rec_df: pd.DataFrame, user_ratings: pd.DataFrame) -> pd.Series:
-        user_ratings['rating'] = user_ratings['rating'].apply(lambda x: x*x)
-        user_movies = movie_rec_df.loc[user_ratings['movieId']]
+    def build_user_profiles(movie_rec_df: pd.DataFrame, user_ratings: pd.DataFrame, norm='l2') -> pd.Series:
+        movie_vectors = movie_rec_df.loc[user_ratings['movieId']].reset_index()
+        movie_vectors['userId'] = user_ratings['userId'].values
+        movie_vectors['rating'] =user_ratings['rating'].values
 
-        # multiply the test_user_movies elements by the test_user_ratings elements
-        profile = user_movies.T.dot(user_ratings['rating'].values)
-        profile[['low_rating', 'medium_rating', 'high_rating']] = profile[['low_rating', 'medium_rating', 'high_rating']] / (2 *profile[['low_rating', 'medium_rating', 'high_rating']].sum())
+        for column in movie_rec_df.columns:
+            movie_vectors[column] = movie_vectors[column] * movie_vectors['rating']
 
-        # normalize all other values
-        norm = profile.sum()
-        for genre in get_genres():
-            profile[genre] = profile[genre] / norm
+        user_profiles = movie_vectors.groupby('userId')[movie_rec_df.columns].sum()
+        user_profiles = user_profiles.div(user_profiles.sum(axis=1), axis=0)
 
-        return profile
+        return user_profiles
